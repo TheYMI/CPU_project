f46306abb2d25f30a90072b7dd6d5367b2d37e20
Yuval Tzur <YTzur@advaoptical.com>
2015-12-09 17:23:22 +0200

    Added implementation of functions and operators to the Value class Moved all members to be private Size is now a member of the object and not part of each base's info

--- src/Simulator/value.py
+++ src/Simulator/value.py
@@ -5,46 +5,102 @@ value.py
 Created by Yuval Tzur on 08/12/2015.
 """
 
+import pprint
+
 
 class Value:
 
     def __init__(self, value=0, size=16, base="bin"):
-        self.__value = value
-        self.__base = base
-        self.__info = {"bin": {"base": 2, "get": self.bin, "size": size},
-                       "int": {"base": 10, "get": self.int},
-                       "hex": {"base": 16, "get": self.hex, "size": size / 4}}
+        self.__info = {"bin": {"base": 2, "size_modifier": 1, "get": self.bin},
+                       "int": {"base": 10, "size_modifier": 1,  "get": self.int},
+                       "hex": {"base": 16, "size_modifier": 4,  "get": self.hex}}
+        self.set_base(base)
+        self.set_size(size)
+        self.set_value(value, base)
 
-    def __str__(self):
-        return str(self.get())
+    __lt__ = lambda self, x: self.__value < int(x)
+    __le__ = lambda self, x: self.__value <= int(x)
+    __eq__ = lambda self, x: self.__value == int(x)
+    __ne__ = lambda self, x: self.__value != int(x)
+    __ge__ = lambda self, x: self.__value >= int(x)
+    __gt__ = lambda self, x: self.__value > int(x)
 
-    def __int__(self):
-        return self.int()
+    __nonzero__ = lambda self: self.__value != 0
+    __bool__ = __nonzero__
 
-    def __hex__(self):
-        return self.hex()
+    get = lambda self: self.__info[self.__base]["get"]()
+    __str__ = lambda self: str(self.get())
+    __getitem__ = lambda self, key: self.__str__()[key]
 
-    def __getitem__(self, item):
-        return self.__str__()[item]
+    bin = lambda self: format(self.__value, "0" + str(self.__size) + "b")[-self.__size:]
+    int = lambda self: self.__value
+    __int__ = int
+    hex = lambda self: format(self.__value, "0" + str(self.get_size("hex")) + "x")[-self.get_size("hex"):].upper()
+    __hex__ = hex
 
-    def get(self):
-        return self.__info[self.__base]["get"]()
+    __abs__ = lambda self: Value(abs(self.__value))
+    __neg__ = lambda self: Value(-self.__value)
+    __pos__ = lambda self: Value(+self.__value)
+    __add__ = lambda self, x: Value(self.__value + int(x))
+    __sub__ = lambda self, x: Value(self.__value - int(x))
+    __mul__ = lambda self, x: Value(self.__value * int(x))
+    __mod__ = lambda self, x: Value(self.__value % int(x))
+    __div__ = lambda self, x: Value(self.__value / int(x))
+    __floordiv__ = lambda self, x: Value(self.__value // int(x))
+    __pow__ = lambda self, x: Value(self.__value ** int(x))
 
-    def set(self, value, base="bin"):
+    __inv__ = lambda self: Value(~self.__value)
+    __invert__ = __inv__
+    __and__ = lambda self, x: Value(self.__value & int(x))
+    __or__ = lambda self, x: Value(self.__value | int(x))
+    __xor__ = lambda self, x: Value(self.__value ^ int(x))
+    __lshift__ = lambda self, x: Value(self.__value << x)
+    __rshift__ = lambda self, x: Value(self.__value >> x)
+
+    get_value = lambda self: self.__value
+    get_base = lambda self: self.__base
+    
+    def __setitem__(self, key, value):
+        new_value = list(self.get())
+        new_value[key] = value
+        self.set_value("".join(new_value), base = self.__base)
+
+    def set_value(self, value, base=None):
+        base = self.__base if not base else base
         value = value if type(value) is int else int(value, int(self.__info[base]["base"]))
         try:
-            self.__value = value % 2**int(self.__info["bin"]["size"])
+            self.__value = value % (2 ** self.__size)
         except KeyError as key_error:
             value_error = ValueError("'" + key_error.message + "' is not a supported base")
             raise value_error
+    
+    set = set_value
 
-    def bin(self):
-        size = int(self.__info["bin"]["size"])
-        return format(self.__value, "0" + str(size) + "b")[-size:]
+    def set_base(self, base):
+        if base in self.__info:
+            self.__base = base
+        else:
+            value_error = ValueError("'" + base + "' is not a supported base")
+            raise value_error
 
-    def int(self):
-        return self.__value
+    def get_size(self, base=None):
+        base = self.__base if not base else base
+        return  (self.__size / self.__info[base]["size_modifier"]) + (self.__size % self.__info[base]["size_modifier"] != 0)
 
-    def hex(self):
-        size = int(self.__info["hex"]["size"])
-        return format(self.__value, "0" + str(size) + "x")[-size:].upper()
+    __len__ = get_size
+
+    def set_size(self, size, base="bin"):
+        if type(size) is not int:
+            raise TypeError("Size must be int")
+
+        self.__size = size * self.__info[base]["size_modifier"]
+
+    def details(self):
+        print "Value: " + str(self.__value)
+        print "Base: " + self.__base
+        print "Size: " + str(self.__size)
+        print "Binary: " + self.bin()
+        print "Decimal: " + str(self.int())
+        print "Hexadecimal: " + self.hex()
+        print "Info:"
+        pprint.pprint(self.__info)
