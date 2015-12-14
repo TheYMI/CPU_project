e2390db7a854040b68afb9d2af7c4b326d5c3e5a
Yuval Tzur <YTzur@advaoptical.com>
2015-12-14 16:40:57 +0200

    bit_value: - Changed the algorithm for ceiling division - set_value now handles int value with bases other than 10 - Updated/added comments

--- src/Simulator/bit_value.py
+++ src/Simulator/bit_value.py
@@ -145,11 +145,15 @@ class BitValue:
 
     def get_size(self, base="bin"):
         size_modifier = int(self.__info[base]["size_modifier"])
-        return (self.__size / size_modifier) + (self.__size % size_modifier != 0)
+        # Do a ceiling division by the modifier for cases in which the number of bits isn't a whole multiple of
+        # the target base
+        # Example: 10 bits is a 3-digit hex number, not 2
+        return ((self.__size - 1) / size_modifier) + 1
 
     # Setters:
     def __setitem__(self, key, value):
         start, stop, step = key.indices(self.__size) if type(key) is slice else (key, key + 1, 1)
+        # Check equality of the size to avoid size changes
         if stop - start != len(value):
             raise ValueError("'" + str(value) + "' has a different length than the range given")
 
@@ -157,10 +161,14 @@ class BitValue:
         new_value[key] = value
         self.set_value("".join(new_value), base="bin")
 
-    def set_value(self, value, base="bin"):
+    def set_value(self, value, base="int"):
         try:
+            # Support base by name and as int value
             base = base if type(base) is int else int(self.__info[base]["base"])
-            value = value if type(value) is int else int(value, base)
+            # Convert the value to a base-10 int for cases in which an int is given with a different base or
+            # a string is given
+            value = int(str(value), base)
+            # Truncate the number according to the number of bits used
             self.__value = value % (2 ** self.__size)
         except KeyError as key_error:
             raise ValueError("'" + str(key_error.message) + "' is not a supported base")
@@ -171,8 +179,10 @@ class BitValue:
         if size < 1:
             raise ValueError("Size can't be smaller than 1")
 
+        # Truncate the number according to the number of bits used if the new size is smaller
+        if size < self.__size:
+            self.__value = self.__value % (2 ** size)
         self.__size = size
-        self.set_value(self.__value)
 
     # Aliases:
     __invert__ = __inv__
